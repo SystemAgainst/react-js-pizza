64369043f2028be28399106586845c1a0d278e8d
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-08-05 02:44:42 +0300

    add: init router and routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './assets/scss/app.scss'
-import {BrowserRouter as Router, createBrowserRouter, Route, RouterProvider, Routes} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-    },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>
 );
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export { default as Home } from './home.jsx';
+export { default as Cart } from './cart.jsx';
