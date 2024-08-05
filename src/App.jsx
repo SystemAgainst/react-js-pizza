cb9636f9f9a4d4f89970fa39e484b322bc800332
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-08-05 03:27:33 +0300

    add: get data from mock-server

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,18 @@ import './App.css'
 import { Header } from "./components/index.js";
 import { Home, Cart } from "./pages/index.js";
 import {Route, Routes} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5173/db.json`)
+            .then((resp) => resp.json())
+            .then((json) => {
+                setItems(json.pizzas);
+            })
+    }, []);
 
   return (
       <>
@@ -11,7 +21,7 @@ function App() {
           <Header />
           <div className="content">
               <Routes>
-                  <Route path="/" element={<Home />} />
+                  <Route path="/" element={<Home items={items} />} />
                   <Route path="/cart" element={<Cart />} />
               </Routes>
           </div>
